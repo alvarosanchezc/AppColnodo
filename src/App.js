c76559db3cc1b57f66b83466dcc7ecad45553976
alvarosanchezc <sanchezcameloalvaro@gmail.com>
2021-11-03 18:23:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Index from "./Components/Index";
+import Layout from "./Components/Layout";
+import Services from "./Components/Services";
+import About from "./Components/AboutUs";
 
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Layout>
+    <Switch>
+          <Route path="/servicios" component={Services}/>
+          <Route path="/nosotros" component={About}/>
+          <Route path="/" component={Index}/>
+    </Switch>
+    </Layout>
+    </BrowserRouter>
   );
 }
-
 export default App;
