7c9acfa301552f1f6f28fc9df610a8c33160d006
alvarosanchezc <sanchezcameloalvaro@gmail.com>
2021-11-06 09:59:49 -0500

    update carrito
    Organizando el codigo

--- src/Components/js/Carrito.jsx
+++ src/Components/js/Carrito.jsx
@@ -1,43 +1,44 @@
 import React from "react";
 
 const Carrito = () => {
+  const [productos, setProducto] = React.useState([]);
+  React.useEffect(() => {
+    obtenerDatos();
+  }, []);
+  //llamar al json
+  const obtenerDatos = async () => {
+    const data = await fetch("api.json");
+    const informacion = await data.json();
+    setProducto(informacion);
+  };
 
-  const [productos, setProducto] = React.useState([])
-
-React.useEffect(() => {
- // console.log('useEffect')
-  obtenerDatos();
-
-},[])
-
-const obtenerDatos = async() =>{
-  const data = await fetch('api.json');
-  const informacion = await data.json();
- // console.log(elementos)
-  setProducto(informacion)
-}
-return (
-
-
-  <div className="row" id="cards">
-      {
-
-
-        productos.map(item =>(
-          
-           <div class="col-12 mb-2 col-md-4">
-        <div class="card">
-             <div class="card-body">
-            <img src={item.thumbnailUrl} alt={"imagen de "+item.title} class="card-img-top"/>
-            <h5>{item.title}</h5>
-            <p>{item.precio}</p>
-            <button class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal1" data-id="1">Comprar</button>
+  return (
+    <div className="row" id="cards">
+      {productos.map((item) => (
+        <div class="col-12 mb-2 col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <img
+                src={item.thumbnailUrl}
+                alt={"imagen de " + item.title}
+                class="card-img-top"
+              />
+              <h5>{item.title}</h5>
+              <p>{item.precio}</p>
+              <button
+                class="btn btn-dark"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal1"
+                data-id="1"
+              >
+                Comprar
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-          ))
-      }
-        </div>
-  )};
+      ))}
+    </div>
+  );
+};
 
 export default Carrito;
