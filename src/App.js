b62e51962650e00aa7b927f973191b24393f168e
alvarosanchezc <sanchezcameloalvaro@gmail.com>
2021-11-10 23:03:16 -0500

    Se revirtieron los cambios

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Menu from "./Components/Menu";
+import Index from "./Components/Index";
+import Layout from "./Components/Layout";
+import Services from "./Components/Services";
+import About from "./Components/AboutUs";
+import Carrito from "./Components/ComponentCarrito";
+import Contacto from "./Components/Contacto";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/Carrito" component={Carrito} />
+          <Route path="/contact" component={Contacto} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/nosotros" component={About} />
+          <Route path="/servicios" component={Services} />
+          <Route path="/" component={Index} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/ComponentCarrito.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Styles/contacto.css";
+import "./Styles/estilos.css";
+import "./Styles/font.css";
+import Modal from "./modal";
+import Cards from "./Cards";
+
+class ComponentCarrito extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+      <Modal/>
+        <div className="container" id="template-cards">
+          <Cards/>
+         </div>
+      </React.Fragment>
+    );
+  }
+}
+export default ComponentCarrito;
